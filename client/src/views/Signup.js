4681f19f7f9e7e55c06b0b9c5fc5bb263b082315
Krisna Sarith <krisnasarith@gmail.com>
2020-11-14 19:05:11 -0800

    signup page

--- client/src/views/Signup.js
+++ client/src/views/Signup.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Form, Input, InputNumber, Button } from 'antd';
+import { Form, Input, InputNumber, Button, Row, Col, Image, Typography, Layout } from 'antd';
 const layout = {
     labelCol: {
-        span: 8,
+        span: 6,
     },
     wrapperCol: {
-        span: 16,
+        span: 18,
     },
 };
 const validateMessages = {
@@ -25,54 +25,70 @@ const Signup = () => {
     };
 
     return (
-        <Form {...layout} name="nest-messages" onFinish={onFinish} validateMessages={validateMessages}>
-            <Form.Item
-                name={['user', 'name']}
-                label="Name"
-                rules={[
-                    {
-                        required: true,
-                    },
-                ]}
-            >
-                <Input />
-            </Form.Item>
-            <Form.Item
-                name={['user', 'email']}
-                label="Email"
-                rules={[
-                    {
-                        type: 'email',
-                    },
-                ]}
-            >
-                <Input />
-            </Form.Item>
-            <Form.Item
-                name={['user', 'age']}
-                label="Age"
-                rules={[
-                    {
-                        type: 'number',
-                        min: 0,
-                        max: 99,
-                    },
-                ]}
-            >
-                <InputNumber />
-            </Form.Item>
-            <Form.Item name={['user', 'website']} label="Website">
-                <Input />
-            </Form.Item>
-            <Form.Item name={['user', 'introduction']} label="Introduction">
-                <Input.TextArea />
-            </Form.Item>
-            <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
-                <Button type="primary" htmlType="submit">
-                    Submit
+        <Row style={{
+            margin: 'auto', padding: '10em 20em', backgroundImage: 'url(https://media2.giphy.com/media/aRZ4vTsHnyW6A/giphy.gif)', backgroundSize: 'cover'
+        }}>
+            <Col span={12}>
+                <Image height='100%' width={'100%'} src="https://www.abf.com.au/wp-content/uploads/2012/09/ABF-new.jpg" preview={false} />
+            </Col>
+            <Col span={12} style={{ backgroundColor: 'white', padding: '5em 5em' }}>
+                <Typography.Title style={{ textAlign: 'center' }}>Join Cardashian</Typography.Title>
+                <Form {...layout} name="nest-messages" onFinish={onFinish} validateMessages={validateMessages}>
+                    <Form.Item
+                        name={['user', 'name']}
+                        label="Username"
+                        rules={[
+                            {
+                                required: true,
+                            },
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item
+                        name={['user', 'alias']}
+                        label="Alias"
+                    >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item
+                        name={['user', 'email']}
+                        label="Email"
+                        rules={[
+                            {
+                                type: 'email',
+                            },
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item
+                        name={['user', 'age']}
+                        label="Age"
+                        rules={[
+                            {
+                                type: 'number',
+                                min: 0,
+                                max: 99,
+                            },
+                        ]}
+                    >
+                        <InputNumber />
+                    </Form.Item>
+                    <Form.Item name={['user', 'website']} label="Website">
+                        <Input />
+                    </Form.Item>
+                    <Form.Item name={['user', 'introduction']} label="Introduction">
+                        <Input.TextArea />
+                    </Form.Item>
+                    <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+                        <Button type="primary" htmlType="submit">
+                            Submit
         </Button>
-            </Form.Item>
-        </Form>
+                    </Form.Item>
+                </Form>
+            </Col>
+        </Row>
     );
 };
 
